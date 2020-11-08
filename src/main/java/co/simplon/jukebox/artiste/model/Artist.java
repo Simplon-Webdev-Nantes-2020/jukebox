c224dbe7e0dc6762c840200603fe8083e701f5c3
manulep <emmanuel.lepevedic.pro@gmail.com>
2020-11-08 18:46:29 +0100

    corr valid first name artist

--- src/main/java/co/simplon/jukebox/artiste/model/Artist.java
+++ src/main/java/co/simplon/jukebox/artiste/model/Artist.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Artist {
@@ -13,7 +13,7 @@ public class Artist {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
-	@NotNull
+	@NotBlank(message = "First name can't be empty")
 	private String firstName;
 
 	private String lastName;
