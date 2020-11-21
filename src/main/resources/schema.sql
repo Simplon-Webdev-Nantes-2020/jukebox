4c619713d0f11cb2845df1bf1c50d3bacd24fecd
manulep <emmanuel.lepevedic.pro@gmail.com>
2020-11-21 15:42:43 +0100

    upgrade database, schema & data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,7 +11,17 @@ CREATE TABLE artist (
 
 CREATE TABLE album (
     id   INTEGER   PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(50) NOT NULL,
+    title VARCHAR(70) NOT NULL,
     release_date DATE,
-    fan_number INTEGER
+    artist_id INT,
+    foreign key (artist_id) references artist(id)
+);
+
+CREATE TABLE track (
+    id   INTEGER   PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    duration INTEGER,
+    preview VARCHAR(200),
+    album_id INT,
+    foreign key (album_id) references album(id)
 );
