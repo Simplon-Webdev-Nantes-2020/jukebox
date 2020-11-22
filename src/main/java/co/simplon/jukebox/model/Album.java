e6ec68d06662a2f211548d9535cda279197cddb7
manulep <emmanuel.lepevedic.pro@gmail.com>
2020-11-22 16:33:33 +0100

    gestion des enfants track dans album (cascade, fetch)

--- src/main/java/co/simplon/jukebox/model/Album.java
+++ src/main/java/co/simplon/jukebox/model/Album.java
@@ -3,7 +3,9 @@ package co.simplon.jukebox.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,6 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
@@ -31,7 +34,7 @@ public class Album {
 	@JsonBackReference
 	private Artist artist;
 	
-	@OneToMany(mappedBy = "album")
+	@OneToMany(mappedBy = "album", cascade = CascadeType.ALL, orphanRemoval=true, fetch=FetchType.EAGER)
 	@JsonManagedReference
 	private List<Track> tracks;
 
