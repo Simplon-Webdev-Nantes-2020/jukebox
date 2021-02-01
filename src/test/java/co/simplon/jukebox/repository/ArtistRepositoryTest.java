70f7aed3c6632537702c0cf1a7c2a43272c163f6
manulep <emmanuel.lepevedic.pro@gmail.com>
2021-02-01 18:08:02 +0100

    TU artist repository

--- src/test/java/co/simplon/jukebox/repository/ArtistRepositoryTest.java
+++ src/test/java/co/simplon/jukebox/repository/ArtistRepositoryTest.java
@@ -1,7 +1,7 @@
 package co.simplon.jukebox.repository;
 
-import java.util.List;
-
+import co.simplon.jukebox.model.Artist;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +9,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
 
-import co.simplon.jukebox.model.Artist;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -22,19 +22,20 @@ public class ArtistRepositoryTest {
      
     @Autowired
     private ArtistRepository repository;
-    
+
+    @DisplayName("find artist by name")
     @Test
     public void findByNameContainingTest( ) {
-    	//given
+    	//GIVEN
     	Artist artistJulian = new Artist("Julian","my bio",120);
     	entityManager.persist(artistJulian);
     	Artist artistMary = new Artist("Mary","my bio",140);
     	entityManager.persist(artistMary);
 
-    	//when
+    	//WHEN
     	List<Artist> artistSelect = repository.findByNameContaining("ul");
 
-    	//then
+    	//THEN
     	assertThat(artistSelect.size()).isEqualTo(1);
     }
 }
