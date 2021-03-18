1a6dcaed8fdc65c86793f9c33162ca47f287d5db
manulep <emmanuel.lepevedic.pro@gmail.com>
2021-03-18 17:25:17 +0100

    security - corr plantage TU

--- src/main/java/co/simplon/jukebox/login/DisableSecurityConfig.java
+++ src/main/java/co/simplon/jukebox/login/DisableSecurityConfig.java
@@ -3,6 +3,8 @@ package co.simplon.jukebox.login;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +19,12 @@ public class DisableSecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .antMatchers("/**").permitAll()
             .anyRequest().authenticated();
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
